db4198fa787f04f0275b0ee18a0dcfcbefca18a7
Scarlett Clark <sgclark@kde.org>
2017-03-09 05:29:10 -0700

    Add libvpx dep

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -16,10 +16,9 @@ else
 fi
 
 if cd ffmpeg; then
-	 ./configure --prefix="/opt/usr" --enable-shared --enable-libmp3lame --enable-gpl --enable-libvorbis --enable-pthreads --enable-libxvid --enable-x11grab --enable-libgsm \
-	 --enable-libx264 --enable-libtheora --enable-libdc1394 --enable-nonfree --disable-stripping --enable-avfilter --enable-libschroedinger --enable-libopencore-amrnb --enable-libopencore-amrwb \
-	 --enable-version3 --enable-libvpx && \
-	  make -j4 && make install		 
+	 ./configure --prefix="/opt/usr" --extra-ldflags="-L/opt/usr//lib -lstdc++ -lm -lrt -ldl" --extra-cflags="-I/opt/usr/include" --enable-shared --enable-avfilter \
+	 --enable-gpl --enable-avresample --enable-libx265 --enable-libvpx --enable-libx264 --disable-doc && \
+	  make -j4 && make install
 else
 	error_exit "$LINENO: An error has occurred.. Aborting."
 fi
